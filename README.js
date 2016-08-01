e95b4bd0e6bab42c26dfbe4d41137a42de3939af
Ed Gauci <ed@gauci.co>
2016-07-31 20:31:43 -0700

    added README

---
+++ README.js
@@ -0,0 +1,11 @@
+Talk Timer
+==========
+
+This project is a talk or speech timer. You set a maximum time in minutes, and optionally
+choose a minimum time and a warning threshold time (both in minutes).
+
+On start, the timer will start counting up. A bar grows from the bottom. On passing the
+"minimum" time, it changes to green. On passing the "warning" time, it changes to
+orange. On hitting the maximum time, it turns red.
+
+The timer can be paused and resumed.
